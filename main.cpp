19c6c1fcd1027a93d4a5df10684fa4d29b03573b
alansam <alansam@users.noreply.github.com>
2023-06-18 03:34:02 +0000

    Add control for logging.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,12 @@
  */
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <sstream>
+
+static
+bool constexpr logging { false };
 
 template <typename T>
 struct tnode {
@@ -16,10 +20,10 @@ struct tnode {
 
   std::string toString(void) const {
     std::ostringstream os;
-    os <<  "\n[@"  << this
-       << " : "    << data
-       << ", L: @" << left
-       << ", R: @" << right
+    os <<  "\n[@"  << std::setw(14) << this
+       << " : "    << std::setw( 5) << data
+       << ", L: @" << std::setw(14) << left
+       << ", R: @" << std::setw(14) << right
        << "]";
     return os.str();
   }
@@ -27,15 +31,19 @@ struct tnode {
 
 template <typename T>
 void insertion(tnode<T> *&  ptr, const T val) {
-  std::clog << "In function " << __func__ << "()\n"
-            << "  arg 1: " << ptr << '\n'
-            << "  arg 2: " << val << '\n';
+  if (logging) {
+    std::clog << "In function " << __func__ << "()\n"
+              << "  arg 1: " << ptr << '\n'
+              << "  arg 2: " << val << '\n';
+  }
   static auto count { 0ul };
   if (ptr == nullptr) {
     ptr = new tnode<T>;
-    std::clog << "    new tnode @" << ptr
-              << " allocate count " << ++count
-              << '\n';
+    if (logging) {
+      std::clog << "    new tnode @" << ptr
+                << " allocate count " << ++count
+                << '\n';
+    }
     ptr->data = val;
     ptr->left = ptr->right = nullptr;
   }
@@ -49,10 +57,14 @@ void insertion(tnode<T> *&  ptr, const T val) {
 
 template <typename T>
 tnode<T> * insert(const T val) {
-  std::clog << "In function " << __func__ << "()\n"
-            << "  arg 1: " << val << '\n';
+  if (logging) {
+    std::clog << "In function " << __func__ << "()\n"
+              << "  arg 1: " << val << '\n';
+  }
   static tnode<T> * root = nullptr;
-  std::clog << "    root: @" << root << '\n';
+  if (logging) {
+    std::clog << "    root: @" << root << '\n';
+  }
   insertion(root, val);
 
   return root;
@@ -60,8 +72,10 @@ tnode<T> * insert(const T val) {
 
 template <typename T>
 void display(const tnode<T> * root) {
-  std::clog << "In function " << __func__ << "()\n"
-            << "  arg 1: " << root << '\n';
+  if (logging) {
+    std::clog << "In function " << __func__ << "()\n"
+              << "  arg 1: " << root << '\n';
+  }
   if (root == nullptr) {
     return;
   }
@@ -72,57 +86,117 @@ void display(const tnode<T> * root) {
   }
 }
 
-
 template <typename T>
 void destroy(const tnode<T> * node) {
-  std::clog << "In function " << __func__ << "()\n"
-            << "  arg 1: " << node << '\n';
+  if (logging) {
+    std::clog << "In function " << __func__ << "()\n"
+              << "  arg 1: " << node << '\n';
+  }
   static auto count { 0ul };
   if (node == nullptr) {
     return;
   }
   else {
     if (node->left != nullptr) {
-      std::clog << "    L: chain  @" << node->left << '\n';
+      if (logging) {
+        std::clog << "    L: chain  @" << node->left << '\n';
+      }
       destroy(node->left);
     }
     if (node->right != nullptr) {
-      std::clog << "    R: chain  @" << node->right << '\n';
+      if (logging) {
+        std::clog << "    R: chain  @" << node->right << '\n';
+      }
       destroy(node->right);
     }
-    std::clog << "    R: delete @" << node
-              << " delete count: " << ++count
-              << '\n';
+    if (logging) {
+      std::clog << "    R: delete @" << node
+                << " delete count: " << ++count
+                << '\n';
+    }
     delete node;
   }
 }
 
 int main(int argc, char const * argv[]) {
-  std::clog << "In function " << __func__ << "()\n"
-            << "  arg 1: " << argc << '\n'
-            << "  arg 2: " << argv << '\n';
+  if (logging) {
+    std::clog << "In function " << __func__ << "()\n"
+              << "  arg 1: " << argc << '\n'
+              << "  arg 2: " << argv << '\n';
+  }
   std::cout << "Tree\n";
 
   tnode<int> * root = nullptr;
 
-  root = insert(20);
-  std::clog << "    " << __func__
-            << " root @" << root << '\n';
-  root = insert( 5);
-  std::clog << "    " << __func__
-            << " root @" << root << '\n';
-  root = insert(67);
-  std::clog << "    " << __func__
-            << " root @" << root << '\n';
-  root = insert( 7);
-  std::clog << "    " << __func__
-            << " root @" << root << '\n';
-  root = insert( 8);
-  std::clog << "    " << __func__
-            << " root @" << root << '\n';
-  root = insert( 5);
-  std::clog << "    " << __func__
-            << " root @" << root << '\n';
+  root = insert( 20);
+  if (logging) {
+    std::clog << "    " << __func__
+              << " root @" << root << '\n';
+  }
+  display(root);
+  std::cout << std::endl;
+
+  root = insert(  5);
+  if (logging) {
+    std::clog << "    " << __func__
+              << " root @" << root << '\n';
+  }
+  display(root);
+  std::cout << std::endl;
+
+  root = insert( 67);
+  if (logging) {
+    std::clog << "    " << __func__
+              << " root @" << root << '\n';
+  }
+  display(root);
+  std::cout << std::endl;
+
+  root = insert(  7);
+  if (logging) {
+    std::clog << "    " << __func__
+              << " root @" << root << '\n';
+  }
+  display(root);
+  std::cout << std::endl;
+
+  root = insert(  8);
+  if (logging) {
+    std::clog << "    " << __func__
+              << " root @" << root << '\n';
+  }
+  display(root);
+  std::cout << std::endl;
+
+  root = insert(  5);
+  if (logging) {
+    std::clog << "    " << __func__
+              << " root @" << root << '\n';
+  }
+  display(root);
+  std::cout << std::endl;
+
+  root = insert(666);
+  if (logging) {
+    std::clog << "    " << __func__
+              << " root @" << root << '\n';
+  }
+  display(root);
+  std::cout << std::endl;
+
+  root = insert(  0);
+  if (logging) {
+    std::clog << "    " << __func__
+              << " root @" << root << '\n';
+  }
+  display(root);
+  std::cout << std::endl;
+
+  root = insert( -1);
+  if (logging) {
+    std::clog << "    " << __func__
+              << " root @" << root << '\n';
+  }
 
   display(root);
   std::cout << std::endl;
